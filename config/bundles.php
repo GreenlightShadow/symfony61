a7c611d1aabcbd087410aa9d7e8cc1b5a9d29353
GreenlightShadow <artyom199levchenko@gmail.com>
2022-12-16 15:18:12 +0200

    [SY61-1.2] Properly configured logs with exception cases that cause errors

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
